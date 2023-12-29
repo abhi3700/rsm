01fb53d912053e66ff7d49688a45ef58661ccc71
abhi3700 <dalvath3700@gmail.com>
2023-12-30 02:16:16 +0530

    Added System Pallet alongwith Runtime initiated, rust configs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 mod balances;
+mod system;
+
+// Runtime
+struct Runtime {
+	system: system::Pallet,
+	balances: balances::Pallet,
+}
+
+impl Runtime {
+	fn new() -> Self {
+		Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
+	}
+}
 
 fn main() {}
---
+++ src/system.rs
@@ -0,0 +1,53 @@
+//! System pallet
+
+use std::collections::BTreeMap;
+
+pub struct Pallet {
+	block_number: u32,
+	nonce: BTreeMap<String, u32>,
+}
+
+impl Pallet {
+	pub fn new() -> Self {
+		Self { block_number: 0, nonce: BTreeMap::new() }
+	}
+
+	fn block_number(&self) -> u32 {
+		self.block_number
+	}
+
+	fn inc_block_number(&mut self) -> Result<(), &'static str> {
+		self.block_number = self.block_number.checked_add(1).ok_or("Exceeded u32 MAX")?;
+
+		Ok(())
+	}
+
+	fn inc_nonce(&mut self, who: &String) -> Result<(), &'static str> {
+		let new_nonce =
+			self.nonce.get(who).unwrap_or(&0).checked_add(1).ok_or("Exceeded u32 MAX")?;
+
+		self.nonce.insert(who.clone(), new_nonce);
+
+		Ok(())
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn init_system() {
+		let mut system = super::Pallet::new();
+
+		// get the genesis block number
+		assert_eq!(system.block_number(), 0);
+		// view alice's nonce at the genesis block
+		assert_eq!(system.nonce.get(&"alice".to_string()), None);
+
+		// inc block & nonce of alice
+		assert!(system.inc_block_number().is_ok());
+		assert!(system.inc_nonce(&"alice".to_string()).is_ok());
+
+		assert_eq!(system.block_number(), 1);
+		assert_eq!(system.nonce.get(&"alice".to_string()), Some(&1));
+	}
+}
