eb8daf15f4a0712ebd9218371ba50f06ee1f9ffb
abhi3700 <dalvath3700@gmail.com>
2023-12-29 22:12:28 +0530

    Added the Balances module along with its tests

---
+++ src/balances.rs
@@ -0,0 +1,74 @@
+use std::collections::BTreeMap;
+
+struct Pallet {
+    balances: BTreeMap<String, u128>,
+}
+
+impl Pallet {
+    fn new() -> Self {
+        Self {
+            balances: BTreeMap::new(),
+        }
+    }
+
+    fn set_balance(&mut self, who: &String, balance: u128) {
+        self.balances.insert(who.clone(), balance);
+    }
+
+    fn balance(&self, who: &String) -> u128 {
+        *self.balances.get(who).unwrap_or(&0)
+    }
+
+    fn transfer(&mut self, from: &String, to: &String, amount: u128) -> Result<(), &'static str> {
+        // old balance - from
+        let from_old_balance = self.balance(from);
+        let to_old_balance = self.balance(to);
+
+        // calculate the new balances
+        let from_new_balance = from_old_balance
+            .checked_sub(amount)
+            .ok_or("Insufficient balance")?;
+
+        let to_new_balance = to_old_balance
+            .checked_add(amount)
+            .ok_or("Exceeds MAX balance")?;
+
+        // set the balances
+        self.set_balance(from, from_new_balance);
+        self.set_balance(to, to_new_balance);
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn init_balances() {
+        let mut balances = super::Pallet::new();
+
+        assert_eq!(balances.balance(&"alice".to_string()), 0);
+        balances.set_balance(&"alice".to_string(), 100);
+        assert_eq!(balances.balance(&"alice".to_string()), 100);
+        assert_eq!(balances.balance(&"bob".to_string()), 0);
+    }
+
+    #[test]
+    fn test_transfer() {
+        let mut balances = super::Pallet::new();
+
+        assert_eq!(
+            balances.transfer(&"alice".to_string(), &"bob".to_string(), 100),
+            Err("Insufficient balance")
+        );
+
+        balances.set_balance(&"alice".to_string(), 100);
+        assert!(balances
+            .transfer(&"alice".to_string(), &"bob".to_string(), 51)
+            .is_ok());
+
+        // check the balances
+        assert_eq!(balances.balance(&"alice".to_string()), 49);
+        assert_eq!(balances.balance(&"bob".to_string()), 51);
+    }
+}
