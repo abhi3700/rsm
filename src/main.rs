a314fae972e40e35e44a28941fd9570f807a8d13
abhi3700 <dalvath3700@gmail.com>
2024-01-03 19:17:35 +0530

    tutorial moved backwards, reserved for Part 3

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,4 @@
 mod balances;
 mod system;
 
-// Runtime
-struct Runtime {
-	system: system::Pallet,
-	balances: balances::Pallet,
-}
-
-impl Runtime {
-	fn new() -> Self {
-		Self { system: system::Pallet::new(), balances: balances::Pallet::new() }
-	}
-}
-
 fn main() {}
